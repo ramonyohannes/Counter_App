64a887243b3b3d5bd6b1f9bd573139e84b51f65e
ramon.yohannes <ramon.yohannes@cyansecurity.com>
2022-08-13 16:09:45 +0300

    Added Folder Structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,16 @@
 import 'package:flutter/material.dart';
 
+import 'presentation/screens/counter_page_screen.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
-        ),
-      ),
+      home: Counter_Screen(),
     );
   }
 }
---
+++ lib/presentation/screens/counter_page_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class Counter_Screen extends StatelessWidget {
+  const Counter_Screen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("data"),
+      ),
+      body: Column(
+        children: [
+          Text("data"),
+          Text("data"),
+          Text("data"),
+        ],
+      ),
+    );
+  }
+}
